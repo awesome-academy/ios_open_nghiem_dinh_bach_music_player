8a900b8d8a8b065cb5249e9102bf04acbf9eb318
Bach <motirelia612@gmail.com>
2023-08-24 08:52:05 +0700

    init

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Bach Nghiem on 24/08/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
